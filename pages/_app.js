91bb79c8ae8fddd71e8a305c8e27b80a5a452cb6
Owen <owen@chessable.com>
2021-05-31 20:40:08 +0100

    another absolute path

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.scss';
+import '@/styles/index.scss';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
 
     return <Component {...pageProps} />
-}  
-export default MyApp
+}
+
+export default MyApp;
