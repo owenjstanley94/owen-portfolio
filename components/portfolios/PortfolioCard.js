46d7c5de810f9c4be30ed1951dcbca5603309aae
Owen <owen@chessable.com>
2021-06-06 15:58:23 +0100

    data rework for PortfolioCard

--- components/portfolios/PortfolioCard.js
+++ components/portfolios/PortfolioCard.js
@@ -2,8 +2,8 @@ const PortfolioCard = ({portfolio}) => {
     return (
             <div className="card subtle-shadow no-border">
                 <div className="card-body">
-                    <h5 className="card-title">{portfolio.title}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">{portfolio.jobTitle}</h6>
+                    <h5 className="card-title">{portfolio.jobTitle}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{portfolio.company}</h6>
                     <p className="card-text fs-2">{portfolio.description}</p>
                 </div>
                 <div className="card-footer no-border">
